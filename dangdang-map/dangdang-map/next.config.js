0ff36ffe5508f1002c4f28f99e38c5388065bb38
luihvf <tnwls030805@gmail.com>
2026-05-12 21:04:33 +0900

    Delete dangdang-map directory

--- dangdang-map/dangdang-map/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/' + (process.env.REPO_NAME || 'dangdang-map') : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' + (process.env.REPO_NAME || 'dangdang-map') + '/' : '',
-  images: { unoptimized: true },
-  transpilePackages: ['maplibre-gl'],
-}
-module.exports = nextConfig
