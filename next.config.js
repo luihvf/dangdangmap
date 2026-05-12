3d879e857fce75d82c1e495bd7085d5ffe120b64
luihvf <tnwls030805@gmail.com>
2026-05-12 21:11:31 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
-const repoName = process.env.REPO_NAME || '';
-
 const nextConfig = {
-  output: 'export',
-  basePath: isGithubPages && repoName ? '/' + repoName : '',
-  assetPrefix: isGithubPages && repoName ? '/' + repoName + '/' : '',
   images: { unoptimized: true },
   transpilePackages: ['maplibre-gl'],
 }
